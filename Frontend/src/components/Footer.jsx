a2efa2bb665afc3d512d4a17a1820fa8ff8d8f83
Likhithanjali9 <anjaliprathapbayanaboina@gmail.com>
2025-04-27 09:24:33 -0700

    footer file is added

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,19 +5,16 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white p-6 mt-10">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <div>
-          <h2 className="font-bold mb-2">Support Adhirath</h2>
-          <p>Means</p>
+          <h2 className="font-bold mb-2">Adhirath</h2>
+          <p>This is a App developed for NEURODEVOLPMENT CHALLENGE KIDS </p>
         </div>
         <div>
           <h2 className="font-bold mb-2">Features</h2>
-          <p>Features Description</p>
+          <p>This App Contains All the Games and Puzzles.</p>
         </div>
         <div>
           <h2 className="font-bold mb-2">Resources</h2>
           <p>resources@email.com</p>
-        </div>
-        <div>
-          <h2 className="font-bold mb-2">Contact</h2>
           <p>Contact Info Here</p>
         </div>
       </div>
